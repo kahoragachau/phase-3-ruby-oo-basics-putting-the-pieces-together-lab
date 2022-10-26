df854f69d8c824ea7ca8cf681f34c72777b718f0
kahoragachau <joelkahoragachau6@gmail.com>
2022-10-26 22:58:05 +0300

    - Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    attr_accessor :turn_page
 
+    def initialize(title)
+        @title = title
+        # @author = author
+        # @page_count = page_count
+        # @turn_page = turn_page
+    end
+    def author=(author)
+        @author = author
+    end
+    def page_count=(page_count)
+        @page_count = page_count
+    end
+    def genre=(genre)
+        @genre = genre
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,33 @@
 # Make your shoe class here!
+
+class Shoe
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def color=(color)
+        @color = color
+    end
+
+    def size=(size)
+        @size=size
+    end
+
+    def material=(material)
+        @material=material
+    end
+
+    def condition=(condition)
+        @condition=condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
